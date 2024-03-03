36cdca3482d6b1a1a9d06270d1038e243caef4d3
ArturArystanbekov <156782799+ArturArystanbekov@users.noreply.github.com>
2024-03-04 00:56:32 +0600

    Add files via upload

---
+++ Lab6/dir and files/Ex6.py
@@ -0,0 +1,9 @@
+import string
+
+def create_files():
+    al = string.ascii_uppercase
+    for let in al:
+        file_name = let + ".txt"
+        with open(file_name, 'w') as file:
+            file.write("This is file " + file_name)
+create_files()
---
+++ Lab6/dir and files/Ex7.py
@@ -0,0 +1,14 @@
+def copy_file(s_f, d_f):
+    try:
+        with open(s_f, 'r') as source:
+            with open(d_f, 'w') as destination:
+                destination.write(source.read())
+        print("File copied successfully")
+    except FileNotFoundError:
+        print("One of the files does not exist")
+    except IOError:
+        print("An error occurred while copying the file")
+    source_file = input()
+    destination_file = input()
+
+    copy_file(source_file, destination_file)
